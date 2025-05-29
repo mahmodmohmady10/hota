2a65c16f4cad1326eafc197fe486d1ed4636dd35
mahmodmohmady10 <36662314+mahmodmohmady10@users.noreply.github.com>
2025-05-29 21:51:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav a");
+  const currentPage = location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
